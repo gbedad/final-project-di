4accaa6b7133a736bd08be6251e0e400a6a39c7f
gbedad <gerald.berrebi@gmail.com>
2022-07-18 22:24:28 +0200

    Set config and import sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+from .auth import auth_blueprint as auth_bp
+from .main import main_blueprint as main_bp
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app():
+def create_app(config_class=Config):
     flask_app = Flask(__name__)
-    from .auth import auth_blueprint as auth_bp
-    from .main import main_blueprint as main_bp
+    flask_app.config.from_object(config_class)
+    db.init_app(flask_app)
+    migrate.init_app(flask_app, db)
     flask_app.register_blueprint(auth_bp)
     flask_app.register_blueprint(main_bp)
+
+    CONFIG_TYPE = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+    flask_app.config.from_object(CONFIG_TYPE)
+
     return flask_app
 
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -5,3 +5,18 @@ from app.auth import auth_blueprint as auth_bp
 @auth_bp.route('/login')
 def login():
     return render_template('auth/login.html', legend='Welcome to login page')
+
+
+@auth_bp.route('/register')
+def register():
+    return render_template('auth/register.html', legend='Welcome to signup page')
+
+
+@auth_bp.route('/reset_request_password')
+def reset_request():
+    return render_template('auth/reset_request_password.html', legend='Request Reset Password')
+
+
+@auth_bp.route('/reset_password')
+def rerset_password():
+    return render_template('auth/reset_password.html', legend='Reset Password')
