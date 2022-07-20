ce9c29916a9f79cd6669205182e6cafca4f028fc
gbedad <gerald.berrebi@gmail.com>
2022-07-20 19:26:21 +0200

    Improve process structure

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectMultipleField, SelectField, widgets, DateField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectMultipleField, SelectField, widgets,\
+    DateField, TextAreaField, validators, IntegerField, IntegerRangeField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Regexp, NumberRange
 from .models import User
 
-CONTRACT_TYPES = [('1', 'Bénévolat'), ('2', 'Service Civique'), ('3', 'Stage'), ('4', 'Prestataire'), ('5', 'Autre')]
+CONTRACT_TYPES = [('1', 'Bénévolat'), ('2', 'Service Civique'), ('3', 'Stage'), ('4', 'Autres')]
 SCHOOL_SUBJECTS = [('5', 'Mathématiques'), ('6', 'Français'), ('7', 'Anglais'),
                    ('8', 'Espagnol'), ('9', 'Allemand'), ('10', 'SVT'),
                    ('11', 'Physique'),('12', 'Histoire-Géographie'), ('13', 'Géopolitique'),]
 SCHOOL_LEVELS = [('1', 'Primaire'), ('2', '6ème'), ('3', '5ème'),
                    ('4', '4ème'), ('5', '3ème'), ('6', 'Seconde'),
                    ('7', 'Première'),('8', 'Terminale'), ('9', 'Sup')]
+
+MODALITIES = [('1', 'Présentiel 12ème'), ('2', 'Présentiel 17ème'), ('3', 'Présentiel Aubervilliers'), ('4', 'Présentiel Autres'), ('5', 'Distanciel'), ('6', 'Hybride')]
 
 
 class RegistrationForm(FlaskForm):
@@ -45,22 +48,37 @@ class ProfilePage1Form(FlaskForm):
     zipcode = StringField('Zipcode', validators=[DataRequired()])
     city = StringField('City', validators=[DataRequired()])
     email2 = StringField('Email 2')
-    phone1 = StringField('Phone 1', validators=[DataRequired()])
-    phone2 = StringField('Phone 2')
-    company = StringField('Establishment', validators=[DataRequired()])
-    activity = StringField('Activity', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired(), Regexp(regex='^[+-]?[0-9]$')])
+    short_text = TextAreaField(u'Please feel free to add any comment', validators=[validators.optional(), validators.length(max=400)])
     birth_date = DateField('Birth Date', format='%d-%m-%Y')
     submit = SubmitField('Save')
 
 
 class ProfilePage2Form(FlaskForm):
-    school_subjects = SelectMultipleField(u'School Subjects', validators=[DataRequired()], choices=SCHOOL_SUBJECTS, coerce=str,option_widget=widgets.CheckboxInput(),widget=widgets.ListWidget(prefix_label=False))
-    school_levels = SelectMultipleField(u'School Levels', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,option_widget=widgets.CheckboxInput(),widget=widgets.ListWidget(prefix_label=False))
-    teaching_terms = StringField('Teaching Terms', validators=[DataRequired()])
+    maths = SelectMultipleField(u'Mathématiques', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,option_widget=widgets.CheckboxInput(),widget=widgets.ListWidget(prefix_label=False))
+    physics = SelectMultipleField(u'Physique', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    french = SelectMultipleField(u'Français', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    english = SelectMultipleField(u'Anglais', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    spanish = SelectMultipleField(u'Espagnol', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    svt = SelectMultipleField(u'SVT', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    history = SelectMultipleField(u'Histoire-Géo', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    geopolitics = SelectMultipleField(u'Géopolitique', validators=[DataRequired()], choices=SCHOOL_LEVELS, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    modalities = SelectMultipleField('Modalities', validators=[DataRequired()], choices=MODALITIES, coerce=str,
+                                option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
     availability_days = StringField('Availability Days')
-    avail = StringField('Phone 1', validators=[DataRequired()])
-    phone2 = StringField('Phone 2')
+    start_date = DateField('Starting Date', format='%Y-%m-%d', validators=[DataRequired()])
+    end_date = DateField('Ending Date', format='%Y-%m-%d', validators=[DataRequired()])
+    frequency = IntegerField('Freq(h/week)', validators=[DataRequired(), NumberRange(min=1, max=6)])
     company = StringField('Establishment', validators=[DataRequired()])
     activity = StringField('Activity', validators=[DataRequired()])
     contract_type = SelectField(u'Contract Type', validators=[DataRequired()], choices=CONTRACT_TYPES)
     submit = SubmitField('Save')
+
+
