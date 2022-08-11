dd680b2e8254dfe6c61db9a50ff972d70f46d6c7
gbedad <gerald.berrebi@gmail.com>
2022-08-11 09:08:26 +0300

    modify users relationship in route profile5 save to db

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -368,11 +368,11 @@ def profile_5(user_name):
 
         if user_uploads:
             if cv_file:
-                user_uploads.cv_filename = secure_filename(cv_file.filename)
+                user_u.cv_filename = secure_filename(cv_file.filename)
             elif b3_file:
-                user_uploads.b3_filename = secure_filename(b3_file.filename)
+                user_u.b3_filename = secure_filename(b3_file.filename)
             elif id_file:
-                user_uploads.id_filename = secure_filename(id_file.filename)
+                user_u.id_filename = secure_filename(id_file.filename)
             try:
                 db.session.commit()
                 flash('File(s) successfully uploaded', 'success')
