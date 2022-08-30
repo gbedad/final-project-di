70f75fa0a9139d635cc5c992e5bf25faf3d3b38c
gbedad <gerald.berrebi@gmail.com>
2022-08-30 18:15:04 +0200

    check user solve error in id

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -276,8 +276,10 @@ def delete_availability(availability_id):
 @login_required
 def delete_modality(modality_id):
     mod_selected = models.Modalities.query.filter_by(id=modality_id).first()
-    tutoring = models.User.query.filter_by(id=mod_selected.user_modality_owner).first()
-    user = models.User.query.filter_by(id=tutoring.user_id)
+    mod_owner = mod_selected.user_modality_owner
+    user_id = models.Tutoring.query.filter_by(id=mod_owner).first()
+
+    user = models.User.query.filter_by(id=user_id)
     db.session.delete(mod_selected)
     db.session.commit()
     return redirect(url_for('auth.profile_2', user_name=current_user.username if current_user.role =='supervisor' else user.username))
