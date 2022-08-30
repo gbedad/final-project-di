ed4d7a51fb0f15b3d43723d90f25871722b6feac
gbedad <gerald.berrebi@gmail.com>
2022-08-30 20:53:00 +0200

    solve deletion of subjects, availabilities and modalities

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -267,9 +267,12 @@ def profile_2(user_name):
 @login_required
 def delete_availability(availability_id):
     day_selected = models.Availabilities.query.filter_by(id=availability_id).first()
+    avail_owner = day_selected.user_availability_owner
+    assoc_tutoring = models.Tutoring.query.filter_by(id=avail_owner).first()
+    user = models.User.query.filter_by(id=assoc_tutoring.user_id).first()
     db.session.delete(day_selected)
     db.session.commit()
-    return redirect(url_for('auth.profile_2', user_name=current_user.username))
+    return redirect(url_for('auth.profile_2', user_name=current_user.username if current_user.role =='supervisor' else user.username))
 
 
 @auth_bp.route('/tutor/modalities/<int:modality_id>/cancel', methods=['GET', 'POST'])
@@ -288,9 +291,12 @@ def delete_modality(modality_id):
 @login_required
 def delete_subject(subject_id):
     subj_selected = models.SubjectsGrades.query.filter_by(id=subject_id).first()
+    sub_owner = subj_selected.user_subjects_owner
+    assoc_tutoring = models.Tutoring.query.filter_by(id=sub_owner).first()
+    user = models.User.query.filter_by(id=assoc_tutoring.user_id).first()
     db.session.delete(subj_selected)
     db.session.commit()
-    return redirect(url_for('auth.profile_2', user_name=current_user.username))
+    return redirect(url_for('auth.profile_2', user_name=current_user.username if current_user.role =='supervisor' else user.username))
 
 
 @auth_bp.route('/user/<user_name>/profile_3', methods=['GET', 'POST'])
