1ed0968f43d646de594050329c744f6b87388582
gbedad <gerald.berrebi@gmail.com>
2022-08-30 16:31:08 +0200

    check user if not current_user

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -276,6 +276,7 @@ def delete_availability(availability_id):
 @login_required
 def delete_modality(modality_id):
     mod_selected = models.Modalities.query.filter_by(id=modality_id).first()
+    user = models.User.query.filter_by(id=mod_selected.user_modality_owner).first()
     db.session.delete(mod_selected)
     db.session.commit()
     return redirect(url_for('auth.profile_2', user_name=current_user.username if current_user.role =='supervisor' else user.username))
