88f14a3526248b970b9a84d9b43fc0a362df2a5f
gbedad <gerald.berrebi@gmail.com>
2022-09-02 15:15:46 +0200

    improve  notification in index

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,10 @@ main_bp = Blueprint('main',  __name__, template_folder='templates', static_folde
 @main_bp.route("/index")
 @login_required
 def index():
-    new_supervisors = models.User.query.filter_by(role='supervisor').filter_by(status='1')
+    #new_supervisors = models.User.query.filter_by(role='supervisor').filter_by(status='1')
+    supervisors = models.User.query.filter_by(role='supervisor')
+    new_supervisors = [created for created in supervisors if created.status == '1']
+    documented_supervisors = [documented for documented in supervisors if documented.status == '3']
     count_new_supervisors = len(list(new_supervisors))
     print(count_new_supervisors)
-    return render_template('main/index.html', data=new_supervisors, num=count_new_supervisors, title='Home page')
+    return render_template('main/index.html', data=new_supervisors, doc=documented_supervisors, num_new=count_new_supervisors, title='Home page')
