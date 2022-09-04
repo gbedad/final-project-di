1d3d7757a3ad8be51ab1397bfb62951b69cf89fb
gbedad <gerald.berrebi@gmail.com>
2022-09-04 14:36:11 +0200

    add search by subject in route

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -55,7 +55,7 @@ def show_tutors():
         tutors = models.User.query.filter_by(role='supervisor').filter(models.User.last_name.contains(q_first)).filter(models.User.last_name.contains(q_last))
     elif q_subject:
         q_subject = q_subject.title()
-        tutors = models.User.query.filter_by(role='supervisor').filter(models.Tutoring.tutor_subjects.has(q_subject))
+        tutors = models.User.query.filter_by(role='supervisor').filter(models.Tutoring.tutor_subjects.has(**q_subject))
     else:
         tutors = models.User.query.filter_by(role='supervisor').order_by(models.User.created_at)
 
