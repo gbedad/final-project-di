b1e7d3a5a2751a317df46bcbd60a691efb5347d4
gbedad <gerald.berrebi@gmail.com>
2022-09-05 12:56:23 +0200

    correct search by subject and  modality

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -60,7 +60,7 @@ def show_tutors():
     elif q_modality:
         tutors = models.User.query.join(models.Tutoring).filter(models.Tutoring.tutor_modalities.any(modality=q_modality))
     elif q_subject != '' and q_modality != '':
-        tutors = db.session.query(models.User).join(models.Tutoring.tutor_subjects).join(models.Tutoring.tutor_modalities).filter(or_(models.Tutoring.tutor_modalities.any(modality=q_modality), models.Tutoring.tutor_subjects.any(subject=q_subject)))
+        tutors = db.session.query(models.User).join(models.Tutoring).filter(models.Tutoring.tutor_modalities.any(modality=q_modality)).filter(models.Tutoring.tutor_subjects.any(subject=q_subject))
     else:
         tutors = models.User.query.filter_by(role='supervisor').order_by(models.User.created_at)
 
